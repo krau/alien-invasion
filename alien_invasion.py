3903fed4745d658034c017ba7dfc14074d1cb15e
krau <krau39@outlook.com>
2022-11-03 14:54:03 +0800

    重构_check_event

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,19 +30,27 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             
-            #控制飞船左右移动
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
     
+    def _check_keydown_events(self,event):
+        '''相应按键按下'''
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self,event):
+        '''相应按键松开'''
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
     def _update_screen(self):
         #重绘屏幕
         self.screen.fill(self.settings.bg_color)
