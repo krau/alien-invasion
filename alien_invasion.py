1c83bf09096ff29d4f51aec3c67bfa1ce73e6c79
krau <krau39@outlook.com>
2022-11-03 14:41:30 +0800

    移动飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,8 +11,8 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         
-        
-        self.screen = pygame.display.set_mode((self.settings.screen_width , self.settings.screen_height))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width , self.settings.screen_height))
         pygame.display.set_caption('外星人入侵')
 
         self.ship = Ship(self)
@@ -20,16 +20,35 @@ class AlienInvasion:
     def run_game(self):
         '''游戏主循环体'''
         while True:
-            #监听鼠标和键盘事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_event()
+            self.ship.update()
+            self._update_screen()
 
-            #重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitem()
-            #使绘制屏幕可见
-            pygame.display.flip()
+    def _check_event(self):
+        #监听鼠标和键盘事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            
+            #控制飞船左右移动
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+    
+    def _update_screen(self):
+        #重绘屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitem()
+        #使绘制屏幕可见
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #当前文件执行，创建游戏实例并开始
--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self,ai_game):
         '''初始化飞船设置'''
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #加载飞船图像，获取外接矩形
@@ -13,6 +14,20 @@ class Ship:
 
         #把新飞船放在底部居中
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #在属性中存储小数
+        self.x = float(self.rect.x)
+        #飞船移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitem(self):
         '''在指定位置绘制飞船'''
